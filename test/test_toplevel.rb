9e8d1243ee011cb654230e553a667e3417e9da3b
TAGOMORI Satoshi <tagomoris@gmail.com>
2018-03-14 16:24:46 +0900

    fix to check executed order

--- test/test_toplevel.rb
+++ test/test_toplevel.rb
@@ -7,17 +7,23 @@ using Deferral::TopLevel
 
 class DeferralTopLevelTest < ::Test::Unit::TestCase
   class Consumer
+    attr_reader :buf
+
     def initialize(resource)
+      @buf = ""
       @r = resource
     end
 
     def yay
-      defer{ @r.close }
-      "do something"
+      @buf << "yay1;"
+      defer{ @r.close; @buf << "closed;" }
+      @buf << "yay2;"
+      defer{ @buf << "deferred2;" }
+      @buf << "yay3;"
     end
 
     def yay!
-      defer{ @r.close }
+      defer{ @r.close; @buf << "closed;" }
       raise "boo"
     end
   end
@@ -27,6 +33,7 @@ class DeferralTopLevelTest < ::Test::Unit::TestCase
     c = Consumer.new(r)
     c.yay
     assert r.closed?
+    assert_equal "yay1;yay2;yay3;deferred2;closed;", c.buf
   end
 
   test 'release a resource in deferred way even for error situation' do
